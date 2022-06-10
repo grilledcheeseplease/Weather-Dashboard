08c70bb480821ccaf00de39deef0fcca4dc1948c
grilledcheeseplease <lacynunnally402@gmail.com>
2022-06-10 18:05:17 -0400

    fixed past search buttons and background. trying to get moment to work now

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var citySrhEl = document.querySelector('#citySrh');
 var srhBtnEl = document.querySelector('#srhBtn');
 var pastSearchEl = document.querySelector('#pastSearch');
 
+// var dateHeader = moment(today).format('dddd');
+// document.getElementById('currentHeader').text(dateHeader);
+
 var apiKey = 'c5f7256db3858e45809dbd56d8c5b72d';
 
 
@@ -51,6 +54,8 @@ function getOneCallWeather(lat, lon) {
 
         document.getElementById('currentWeather').append(tempEl, windEl, uvEl, humidityEl);
         document.getElementById('listGroup').innerHTML = ''; 
+
+        
         for(var i = 0; i < 5; i++) {
             
             var day = data.daily[i];
@@ -66,7 +71,7 @@ function getOneCallWeather(lat, lon) {
             var uvEl = document.createElement('li');
             var humidityEl = document.createElement('li');
             var forecastCard = document.createElement('div');
-
+            
             lowTemp.textContent = "MIN: " + minEl;
             forecastCard.classList.add('card');
             highTemp.textContent = "MAX: " + maxEl;
@@ -82,9 +87,9 @@ function getOneCallWeather(lat, lon) {
             windEl.classList.add('list-group-item');
             uvEl.classList.add('list-group-item');
             humidityEl.classList.add('list-group-item');
-
+            
             document.getElementById('listGroup').append(forecastCard);
-       
+            
         }
     });
 }
@@ -93,7 +98,7 @@ var searchHistory = function(city) {
 
     var searchHistoryEl = document.createElement('button');
     searchHistoryEl.textContent = city;
-    searchHistoryEl.classList = "d-flex w-100 btn-light border p-2";
+    searchHistoryEl.classList = "btn btn-secondary btn-lg btn-block mt-2";
     searchHistoryEl.addEventListener('click', function(event) {
         var searchBtnValue = event.target.textContent;
         geoCall(searchBtnValue);
