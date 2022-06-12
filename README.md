155a7abf3808ce44d68aacf34b62be01ebbc57a8
grilledcheeseplease <lacynunnally402@gmail.com>
2022-06-11 23:04:27 -0400

    finished readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Weather-Dashboard
+
+## Table of Contents 
+- [Description](#description)
+- [Visuals](#visuals)
+- [Resources](#resources)
+
+## Description
+
+Here we have a weather dashboard that will allow you to search your city and display the current weather along with a following 5 day forecast. The weather will display the day of the week along with an icon to display the weather condition. Along with providing the temperatures, you will also see the wind speed, uv index, and humidity. 
+
+The uv index will change color depending on the intensity of the index that day. This site also utilized local storage by saving your past searches and displaying them below the search section. I have put a lot of time and love into this and I hope you enjoy your search!
+
+- [Click to navigate back to the Table of Contents](#table-of-contents)
+
+## Visuals
+
+![Weather Dashboard](/images/Weather%20Dashboard.mp4)
+
+- [Click to navigate back to the Table of Contents](#table-of-contents)
+
+## Resources
+- [Live Site](https://grilledcheeseplease.github.io/Weather-Dashboard/)
+- [Repository](https://github.com/grilledcheeseplease/Weather-Dashboard)
+
+
+- [Click to navigate back to the Table of Contents](#table-of-contents)
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+// global variables 
 var citySrhEl = document.querySelector('#citySrh');
 var srhBtnEl = document.querySelector('#srhBtn');
 var pastSearchEl = document.querySelector('#pastSearch');
 
 var apiKey = 'c5f7256db3858e45809dbd56d8c5b72d';
 
-
+// function for pulling past searches from local storage 
 function loadPastSearches () {
     var pastCities = JSON.parse(localStorage.getItem('cities')) || [];
     for(let city of pastCities) {
         searchHistory(city);
     }
 }
-
+// function to call geo api for lat and lon
 function geoCall(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     .then(function (response) {
@@ -21,7 +22,7 @@ function geoCall(city) {
         getOneCallWeather(data.coord.lat, data.coord.lon);
     });
 }
-
+// function to take in lat and lon and provide data needed for weather display
 function getOneCallWeather(lat, lon) {
     var apiCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     
@@ -31,13 +32,13 @@ function getOneCallWeather(lat, lon) {
         
         return response.json();
     })
-    
+    // search through data returned from call 
     .then(function(data) {
         document.getElementById('currentWeather').innerHTML = '';
-        
+        // moment for date display
         var dateHeader = moment().format('dddd');
         document.getElementById('currentHeader').textContent = dateHeader;
-
+        // all data variables needed and creating elements for current weather
         var temp = data.current.temp;
         var wind_speed = data.current.wind_speed;
         var uvi = data.current.uvi;
@@ -48,17 +49,17 @@ function getOneCallWeather(lat, lon) {
         var windEl = document.createElement('p');
         var uvEl = document.createElement('p');
         var humidityEl = document.createElement('p');
-        
+        // calling icon from url
         currentImg.setAttribute('src', `https://openweathermap.org/img/wn/${currentIcon}@4x.png`);
         tempEl.textContent = "TEMP: " + temp + " °F";
         windEl.textContent = "WIND: " + wind_speed;
         uvEl.textContent = "UV: " + uvi;
         humidityEl.textContent = "HUMIDITY: " + humidity + " %";
-        
+        // setting uv index color based on what uv is 
         var uviClass = 'btn-success';
         if(uvi >= 3){uviClass = 'btn-warning'};
         if(uvi >= 8){uviClass = 'btn-danger'};
-
+        // adding classes to elements 
         uvEl.setAttribute('class', 'btn-active');
         uvEl.setAttribute('class', uviClass);
         document.getElementById('todayDiv').setAttribute('class', 'card text-center');
@@ -66,10 +67,9 @@ function getOneCallWeather(lat, lon) {
         document.getElementById('currentHeader').append(currentImg);
         document.getElementById('currentWeather').append(tempEl, windEl, uvEl, humidityEl);
         document.getElementById('listGroup').innerHTML = ''; 
-        
-        
+        // loop for 5 day forecast
         for(var i = 0; i < 5; i++) {
-            
+            // variables needed from data with day having i + 1 so starts day after current
             var day = data.daily[i + 1];
             var dateHeader = moment(day.dt, 'X').format('dddd');
             var minEl = day.temp.min;
@@ -88,7 +88,7 @@ function getOneCallWeather(lat, lon) {
             var uvEl = document.createElement('li');
             var humidityEl = document.createElement('li');
             var forecastCard = document.createElement('div');
-            
+            // pulling icon and setting element text contents
             dailyImg.setAttribute('src', `https://openweathermap.org/img/wn/${dailyIcon}.png`);
             dailyHeader.textContent = dateHeader + " ";
             imgSpan.append(dailyImg);
@@ -96,10 +96,11 @@ function getOneCallWeather(lat, lon) {
             highTemp.textContent = "MAX: " + maxEl + " °F";
             windEl.textContent = "WIND: " + wind_speed;
             uvEl.textContent = "UV: " + uvi;
-            humidityEl.textContent = "HUMIDITY: " + humidity + " %";
+            humidityEl.textContent = "HUMIDITY: " + humidity;
+            // appending all elements to to list and list to card
             forecastList.append(dailyHeader, imgSpan, lowTemp, highTemp, windEl, uvEl, humidityEl);
             forecastCard.append(forecastList);
-
+            // setting classes for elements 
             forecastCard.setAttribute('class', 'card')
             forecastList.setAttribute('class', 'list-group list-group-flush text-center');
             forecastList.setAttribute('style', 'font-weight:bold')
@@ -108,23 +109,20 @@ function getOneCallWeather(lat, lon) {
             windEl.classList.add('list-group-item');
             uvEl.classList.add('list-group-item');
             humidityEl.classList.add('list-group-item');
-            
+            // uv index based on what it is that day
             var uviClass = 'btn-success';
             if(uvi >= 3){uviClass = 'btn-warning'};
             if(uvi >= 8){uviClass = 'btn-danger'};
 
             uvEl.setAttribute('class', 'btn-active');
             uvEl.setAttribute('class', uviClass);
-
+            // appending forecastCard to listGroup
             document.getElementById('listGroup').append(forecastCard);
-           
-
-            
             
         }
     });
 }
-
+// function for city search history that turns searches into buttons
 var searchHistory = function(city) {
 
     var searchHistoryEl = document.createElement('button');
@@ -142,14 +140,13 @@ var searchHistory = function(city) {
 
 loadPastSearches ();
 
-// event handlers here
+// event handlers here for when search button is clicked 
 $('#srhBtn').on('click', function() {
     geoCall($('#citySrh').val());
     var currentCities = JSON.parse(localStorage.getItem('cities')) || [];
     currentCities.push($('#citySrh').val());
     localStorage.setItem('cities', JSON.stringify(currentCities));
     searchHistory($('#citySrh').val());
-    // getOneCallWeather('');
 });
 
 
